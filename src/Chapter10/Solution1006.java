2396e0cbd416b67ac1689a7e0ac3ca4b88a5c393
lihao98722 <lihao98722@gmail.com>
2015-09-24 23:06:15 -0400

    Update 10.6

--- src/Chapter10/Solution1006.java
+++ src/Chapter10/Solution1006.java
@@ -5,7 +5,8 @@ package Chapter10;
  */
 public class Solution1006 {
 
-    /* Obviously, all of the data can not be loaded into memory together, so we can not use in-memory sorting algorithms.
+    /*
+     * Obviously, the whole data can not be loaded into memory together, so we can not use in-memory sorting algorithms.
      * we should divide the whole data set into several smaller data sets(depends on the memory limits), then sort each
      * smaller data set in memory to make them in order.
      * Then use external sort(maintain a heap in memory to combine-sort these small data sets) to get fully sorted file.
