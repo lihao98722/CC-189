ed6a4c18590feb156dbfb3ca42dd8882d73f5c06
lihao98722 <lihao98722@gmail.com>
2015-10-02 16:19:36 -0400

    Done 5.5

---
+++ src/Chapter5/Solution55.java
@@ -0,0 +1,18 @@
+package Chapter5;
+
+/**
+ * Created by Hao on 10/2/15.
+ */
+public class Solution55 {
+
+    // ((n & (n - 1)) == 0) means n is an integer that has exactly one 1 in its binary form.
+
+    public static void main(String[] args) {
+        int num = 100;
+        for (int n = 0; n < num; ++n) {
+            if ((n & (n - 1)) == 0) {
+                System.out.print(n + " ");
+            }
+        }
+    }
+}
